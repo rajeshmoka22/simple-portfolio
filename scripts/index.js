416b57a2dd32540ef18e7ba7d411508eacf5b735
Rajesh Moka <rajeshmoka@icloud.com>
2020-05-03 09:06:50 +0530

    folder strcture and dark mode added

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+let style = window.getComputedStyle(document.body);
+let root = document.documentElement;
+document.getElementById('darkMode').addEventListener('click',function(){
+    var bgColor = style.getPropertyValue('--bg-color');
+    var fontColor = style.getPropertyValue('--font-color');
+    root.style.setProperty('--bg-color', bgColor.trim()=='white'? 'black':'white');
+    root.style.setProperty('--font-color', fontColor.trim()=='white'? 'black':'white');
+    this.innerHTML = this.innerHTML=="Switch to light mode"? "Switch to dark mode": "Switch to light mode";
+});
